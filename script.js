b7a0affa686523622fadd0f732c78bd655458830
Rafael Riscado <RafaelRiscado@protonmail.com>
2024-06-29 23:35:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const flowers = document.querySelectorAll(".flower");
-    flowers.forEach((flower, index) => {
-        flower.style.animationDelay = `${index * 0.5}s`;
-        flower.style.width = "20px";
-        flower.style.height = "20px";
-    });
-});
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
